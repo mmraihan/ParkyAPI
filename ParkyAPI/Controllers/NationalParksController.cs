06b8d4ffa0cf7ec482bf3edb270731273aac27e1
mmraihan <mm_raihan@live.com>
2021-12-30 20:12:01 +0600

    NP API: Get Data From Db (GetNationalParks).

--- ParkyAPI/Controllers/NationalParksController.cs
+++ ParkyAPI/Controllers/NationalParksController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ParkyAPI.Models.Dtos;
+using ParkyAPI.Repository.IRepository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +14,31 @@ namespace ParkyAPI.Controllers
     [ApiController]
     public class NationalParksController : Controller
     {
+        private readonly INationalParkRepository _npRepo;
+        private readonly IMapper _mapper;
+
+        public NationalParksController(INationalParkRepository npRepo, IMapper mapper)
+        {
+            _npRepo = npRepo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+
+        public IActionResult GetNationalParks()
+        {
+            var objListFromDb = _npRepo.GetNationalParks();
+
+            //Should not expose Domain Models to ouside, Should expose the DTO Models
+
+            var objDto = new List<NationalParkDto>();
+
+            foreach (var obj in objListFromDb)
+            {
+               objDto.Add(_mapper.Map<NationalParkDto>(obj)); //to=NatioNalParkDto, from = obj                           
+            }
+            return Ok(objDto);
+        }
+
     }
 }
