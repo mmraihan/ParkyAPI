5e3760c9a91e5cfbca715bf8a25914b6a70493dd
mmraihan <mm_raihan@live.com>
2022-01-02 13:52:00 +0600

    Trails: Add Trail Repository and Interface.

---
+++ ParkyAPI/Repository/IRepository/ITrailRepository.cs
@@ -0,0 +1,20 @@
+﻿using ParkyAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Repository.IRepository
+{
+    public interface ITrailRepository
+    {
+        ICollection<Trail> GetTrails();
+        Trail GetTrail(int traild);
+        bool TrailExists(string name);
+        bool TrailExists(int id);
+        bool CreateTrail(Trail trail);
+        bool UpdateTrail(Trail trail);
+        bool DeleteTrail(Trail trail);
+        bool Save();
+    }
+}
---
+++ ParkyAPI/Repository/TrailRepository.cs
@@ -0,0 +1,68 @@
+﻿using ParkyAPI.Data;
+using ParkyAPI.Models;
+using ParkyAPI.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Repository
+{
+    public class TrailRepository : ITrailRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public TrailRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public bool CreateTrail(Trail trail)
+        {
+            _db.Trails.Add(trail);
+            return Save();
+        }
+
+        public bool DeleteTrail(Trail trail)
+        {
+            _db.Trails.Remove(trail);
+            return Save();
+        }
+
+        public Trail GetTrail(int trailId)
+        {
+            var data= _db.Trails.FirstOrDefault(c => c.Id == trailId);
+            return data;
+        }
+
+        public ICollection<Trail> GetTrails()
+        {
+            var listOfDataFromDb = _db.Trails.OrderBy(c => c.Name).ToList();
+            return listOfDataFromDb;
+        }
+
+        public bool TrailExists(string name)
+        {
+            bool value = _db.Trails.Any(a => a.Name.ToLower().Trim() == name.ToLower().Trim());
+            return value;
+        }
+
+        public bool TrailExists(int id)
+        {
+            bool value = _db.Trails.Any(a => a.Id == id);
+            return value;
+        }
+
+        public bool Save()
+        {
+            return _db.SaveChanges() >= 0 ? true : false;
+        }
+
+        public bool UpdateTrail(Trail trail)
+        {
+            _db.Trails.Update(trail);
+            return Save();
+        }
+    }
+}
