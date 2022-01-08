e4530a2c85bcc20cdc7bb2c8aa3b0208516b3c23
mmraihan <mm_raihan@live.com>
2022-01-08 16:26:34 +0600

    NP Web:  Index Action Method.

---
+++ ParkyWeb/Controllers/NationalParksController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParkyWeb.Models;
+using ParkyWeb.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Controllers
+{
+    public class NationalParksController : Controller
+    {
+        private readonly INationalParkRepository _np;
+
+        public NationalParksController(INationalParkRepository np)
+        {
+            _np = np;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetAllNationalParks()
+        {
+            return Json(new { data = await _np.GetAllAsync(SD.NationalParkApiPath) });
+        }
+    }
+}
