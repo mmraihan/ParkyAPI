be6f4e941f859a4b1ca8f38c2c7df76f15583a39
mmraihan <mm_raihan@live.com>
2022-01-09 18:15:44 +0600

    NP Web: Upsert POST Action (Image with aall Propeties).

--- ParkyWeb/Controllers/NationalParksController.cs
+++ ParkyWeb/Controllers/NationalParksController.cs
@@ -3,6 +3,7 @@ using ParkyWeb.Models;
 using ParkyWeb.Repository.IRepository;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -42,6 +43,47 @@ namespace ParkyWeb.Controllers
             }
             return View(obj);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(NationalPark obj)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count>0)
+                {
+                    byte[] p1 = null;
+                    using (var fs1 = files[0].OpenReadStream())
+                    {
+                        using(var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }                       
+                    }
+                    obj.Picture = p1;
+                }
+                else
+                {
+                    var objFromDb = await _np.GetAsync(SD.NationalParkApiPath, obj.Id);
+                    obj.Picture = objFromDb.Picture;
+                }
+                if (obj.Id==0)
+                {
+                    await _np.CreateAsync(SD.NationalParkApiPath, obj);
+                }
+                else
+                {
+                    await _np.UpdateAsync(SD.NationalParkApiPath + obj.Id, obj);
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(obj);
+        }
+            
 
     }
 }
