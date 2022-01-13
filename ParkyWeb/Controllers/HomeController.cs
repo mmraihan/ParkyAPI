be76b9b46f1ae7d1713f73134a5e864c5055e4a8
mmraihan <mm_raihan@live.com>
2022-01-13 13:59:46 +0600

    Home Page:  Home Controller and Index View Model.

--- ParkyWeb/Controllers/HomeController.cs
+++ ParkyWeb/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ParkyWeb.Models;
+using ParkyWeb.Models.ViewModel;
+using ParkyWeb.Repository.IRepository;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -12,15 +14,25 @@ namespace ParkyWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly INationalParkRepository _npRepo;
+        private readonly ITrailRepository _trailRepo;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, INationalParkRepository npRepo, ITrailRepository trailRepo)
         {
             _logger = logger;
+            _npRepo = npRepo;
+            _trailRepo = trailRepo;
         }
 
-        public IActionResult Index()
+        public async Task <IActionResult> Index()
         {
-            return View();
+            var listOfParksAndTrails = new IndexVM()
+            {
+                NationalParkList = await _npRepo.GetAllAsync(SD.NationalParkApiPath),
+                TrailList = await _trailRepo.GetAllAsync(SD.TrailApiPath)
+            };
+
+            return View(listOfParksAndTrails);
         }
 
         public IActionResult Privacy()
