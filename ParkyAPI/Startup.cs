95f3bdcfc856d14cd423b787954a953dd5ad5dc5
mmraihan <mm_raihan@live.com>
2022-01-13 21:30:06 +0600

    Auth API: Add User Repository withou logic.

--- ParkyAPI/Startup.cs
+++ ParkyAPI/Startup.cs
@@ -41,6 +41,7 @@ namespace ParkyAPI
                (options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddScoped<INationalParkRepository, NationalParkRepository>();
             services.AddScoped<ITrailRepository, TrailRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
             services.AddAutoMapper(typeof(ParkyMapings));
             services.AddApiVersioning(options =>
             {
